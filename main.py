528f079e068e8706e946d867750cedada46b5b98
B-Lukky <bas.lukkien@outlook.com>
2025-04-09 18:26:07 +0200

    Bookbot finished!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from stats import *
+from stats import count_words, count_chars
+import sys
+
 
 def get_book_text(book_path):
     with open(book_path) as book:
@@ -6,7 +8,34 @@ def get_book_text(book_path):
 
     return book_content
 
+
+def get_book_filepath():
+    # file_path = ""
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        return file_path
+
+
 def main():
-    print(f"{count_words(get_book_text("books/frankenstein.txt"))} words found in the document")
+    book_path = get_book_filepath()
+    frankenstein = get_book_text(book_path)
+
+    total_words = count_words(frankenstein)
+    unique_words_sorted = count_chars(frankenstein)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for char_dict in unique_words_sorted:
+        char = char_dict["name"]
+        count = char_dict["num"]
+        print(f"{char}: {count}")
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,22 @@ def count_words(book_content):
     for word in words_str:
         word_count += 1
     return word_count
+
+
+def count_chars(book_str):
+    char_lib = {}
+    book_lowercase = book_str.lower()
+    for char in book_lowercase:
+        if char.isalpha():
+            if char in char_lib:
+                char_lib[char] += 1
+            else:
+                char_lib[char] = 1
+
+    sorted_list = []
+    for char, count in char_lib.items():
+        sorted_list.append({"name": char, "num": count})
+
+    sorted_list.sort(reverse=True, key=lambda x: x["num"])
+
+    return sorted_list
